d60f4fe76e3b5ad7008fbb47b7e88a7cae670dfe
Alexey Lang <alekseylang@gmail.com>
2012-07-04 13:04:05 +0400

    phing.php: set absolute path to our phing.

--- generator/bin/phing.php
+++ generator/bin/phing.php
@@ -12,6 +12,7 @@
 // Set any INI options for PHP
 // ---------------------------
 
+set_include_path(get_include_path() . PATH_SEPARATOR . '/usr/local/Cellar/php/5.3.10/lib/php');
 $dirname = dirname(__FILE__);
 $autoloaded = false;
 foreach (array($dirname . '/../../', $dirname . '/../../../../../') as $dir) {
